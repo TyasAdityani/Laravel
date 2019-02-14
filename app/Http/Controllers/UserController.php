c1738e718de068aa0948dc727caada8536205d7f
TyasAdityani <tyasadityani18@gmail.com>
2019-02-14 07:33:43 +0700

    video Eps-13

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,6 +20,14 @@ class UserController extends Controller
     }
     public function save(Request $req)
     {
+        \Validator::make($req->all(),[
+            'name'=>'required|between:3,100',
+            'email'=>'required|unique:users,email',
+            'password'=>'required|min:6',
+            'repassword'=>'required|same:password',
+            'akses'=>'required',
+        ])->validate();
+
     	return 'Fungsi Save';
     }
 }
--- resources/views/admin/pages/user/add.blade.php
+++ resources/views/admin/pages/user/add.blade.php
@@ -17,43 +17,60 @@
 					
 					<div class="form-group form-label-group">
 						<input type="text" name="name"
-						class="form-control"
+						class="form-control {{ $errors->has('name')?'is-invalid':'' }}"
 						value="{{ old('name')}}"
 						id="iName" placeholder="Name" required>
 						<label for="iName">Name</label>
+						@if($errors->has('name'))
+						<div class="invalid-feedback">{{ $errors->first('name') }}</div>
+						@endif
 					</div><!-- End Form Group -->
 
 					<div class="form-group form-label-group">
 						<input type="email" name="email"
-						class="form-control"
+						class="form-control {{ $errors->has('email')?'is-invalid':'' }}"
 						value="{{ old('email')}}"
 						id="iEmail" placeholder="Email" required>
 						<label for="iEmail">Email</label>
+						@if($errors->has('email'))
+						<div class="invalid-feedback">{{ $errors->first('email') }}</div>
+						@endif
 					</div><!-- End Form Group -->
 
 					<div class="form-group form-label-group">
 						<input type="password" name="password"
-						class="form-control"
+						class="form-control {{ $errors->has('password')?'is-invalid':'' }}"
 						value="{{ old('password')}}"
 						id="iPassword" placeholder="Password" required>
 						<label for="iPassword">Password</label>
+						@if($errors->has('password'))
+						<div class="invalid-feedback">{{ $errors->first('password') }}</div>
+						@endif
 					</div><!-- End Form Group -->
 
 					<div class="form-group form-label-group">
 						<input type="password" name="repassword"
-						class="form-control"
-						value="{{ old('repassword')}}"
+						class="form-control {{ $errors->has('repassword')?'is-invalid':'' }}"
 						id="iRePassword" placeholder="Re Password" required>
 						<label for="iRePassword">Re Password</label>
+						@if($errors->has('repassword'))
+						<div class="invalid-feedback">{{ $errors->first('repassword') }}</div>
+						@endif
 					</div><!-- End Form Group -->
 
 					<div class="form-group form-label-group">
-						<select class="form-control"
+						<?php 
+						$val =old('akses');
+						 ?>
+						<select class="form-control {{ $errors->has('akses')?'is-invalid':'' }}"
 						name="akses">
-							<option value="">Pilih Akses Sebagai :</option>
-							<option value="operator">Operator</option>
-							<option value="admin">Administrator</option>
+							<option value="" {{ $val==""?'selected':'' }}>Pilih Akses Sebagai :</option>
+							<option value="operator" {{ $val=="operator"?'selected':'' }}>Operator</option>
+							<option value="admin" {{ $val=="admin"?'selected':'' }}>Administrator</option>
 						</select>
+						@if($errors->has('akses'))
+						<div class="invalid-feedback">{{ $errors->first('akses') }}</div>
+						@endif
 					</div><!-- End Form Group -->
 
 				</div><!--End Card Body-->
