9df9df0d65f603ba3d5b22bb69f81a733889c438
TyasAdityani <tyasadityani18@gmail.com>
2019-02-17 06:56:38 +0700

    video Eps-17

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,6 +59,27 @@ class UserController extends Controller
             'akses'=>'required',
         ])->validate();
 
-    	return 'Fungsi Update';
+        if(!empty($req->password)){
+            $field=[
+                'name'=>$req->name,
+                'email'=>$req->email,
+                'akses'=>$req->akses,
+                'password'=>bcrypt($req->password),
+            ];
+        }else {
+             $field=[
+                'name'=>$req->name,
+                'email'=>$req->email,
+                'akses'=>$req->akses,
+            ];
+        }
+
+        $result= User::where('id',$req->id)->update($field);
+
+        if($result){
+            return redirect()->route('admin.user')->with('result','update');
+        } else {
+            return back()->with('result','fail');
+        }
     }
 }
